13342a206fd58016d900912658953b4c7bc5cdbe
G9 Aerospace <g9militants@gmail.com>
2024-03-04 21:56:25 +0530

    furnished

--- commands/tagcreate.js
+++ commands/tagcreate.js
@@ -8,31 +8,47 @@ module.exports = {
     description: 'Create a new tag',
   },
   async execute(interaction) {
-    // Create the modal
-    const tagCreateModal = new ModalBuilder()
-      .setCustomId('tagCreateModal')
-      .setTitle('Create a New Tag');
+    try {
+      // Check if the user has the required administrative role
+      const requiredRoleID = process.env.ADMINISTRATIVE_ROLE_ID;
+      if (!interaction.member.roles.cache.has(requiredRoleID)) {
+        // User doesn't have the required role
+        return await interaction.reply({
+          content: 'You do not have permission to use this command.',
+          ephemeral: true, // This makes the response visible only to the user who executed the command
+        });
+      }
 
-    // Create the text input components
-    const titleInput = new TextInputBuilder()
-      .setCustomId('titleInput')
-      .setLabel('Enter the title for the tag:')
-      .setStyle(TextInputStyle.Short)
-      .setRequired(true);
+      // Create the modal
+      const tagCreateModal = new ModalBuilder()
+        .setCustomId('tagCreateModal')
+        .setTitle('Create a New Tag');
 
-    const contentInput = new TextInputBuilder()
-      .setCustomId('contentInput')
-      .setLabel('Enter the content for the tag:')
-      .setStyle(TextInputStyle.Paragraph)
-      .setRequired(true);
+      // Create the text input components
+      const titleInput = new TextInputBuilder()
+        .setCustomId('titleInput')
+        .setLabel('Enter the title for the tag:')
+        .setStyle(TextInputStyle.Short)
+        .setRequired(true);
 
-    // Add inputs to the modal with separate action rows
-    const titleActionRow = new ActionRowBuilder().addComponents(titleInput);
-    const contentActionRow = new ActionRowBuilder().addComponents(contentInput);
+      const contentInput = new TextInputBuilder()
+        .setCustomId('contentInput')
+        .setLabel('Enter the content for the tag:')
+        .setStyle(TextInputStyle.Paragraph)
+        .setRequired(true);
 
-    tagCreateModal.addComponents(titleActionRow, contentActionRow);
+      // Add inputs to separate action rows
+      const titleActionRow = new ActionRowBuilder().addComponents(titleInput);
+      const contentActionRow = new ActionRowBuilder().addComponents(contentInput);
 
-    // Show the modal to the user
-    await interaction.showModal(tagCreateModal);
+      // Add action rows to the modal
+      tagCreateModal.addComponents(titleActionRow, contentActionRow);
+
+      // Show the modal to the user
+      await interaction.showModal(tagCreateModal);
+    } catch (error) {
+      console.error(error);
+      await interaction.reply({ content: 'An error occurred while processing the command.', ephemeral: true });
+    }
   },
 };
